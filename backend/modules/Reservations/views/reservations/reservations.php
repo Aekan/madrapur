8453871ff6bcf0ff3a89e4227d64e73903c8270f
Kakukk89ps <kakukk89.ps@gmail.com>
2019-06-25 16:46:46 +0200

    * new React screen

---
+++ backend/modules/Reservations/views/reservations/reservations.php
@@ -0,0 +1,26 @@
+<div id="root"></div>
+
+<script type="text/babel">
+    class YiiReservations extends React.Component {		
+        constructor(props) {
+            super(props);
+
+            this.state = {
+                
+            };
+        }
+
+        render() {
+            return (
+                <div className="container">
+                    here comes the sun
+                </div>           			
+            );              
+        }	
+    }
+
+    ReactDOM.render(
+        <YiiReservations />,
+        document.getElementById('root')
+    );
+</script>
